50b4b7cf89e2f0211b0c2fb5b2a36cabac903d51
chengreg <644076531@qq.com>
2025-07-23 15:59:44 -0400

    Set Up Hero Character: Hero Anim Instance

--- Source/Warrior/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
+++ Source/Warrior/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
@@ -2,4 +2,33 @@
 
 
 #include "AnimInstances/Hero/WarriorHeroAnimInstance.h"
+#include "Characters/WarriorHeroCharacter.h"
 
+void UWarriorHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	// 若成功获取基础角色，则尝试转为英雄角色
+	if (OwningCharacter)
+	{
+		OwningHeroCharacter = Cast<AWarriorHeroCharacter>(OwningCharacter);
+	}
+}
+
+void UWarriorHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	// 如果角色有加速度（移动中），则重置计时器和状态
+	if (bHasAcceleration)
+	{
+		IdleElapsedTime = 0.0f;
+		bShouldEnterRelaxState = false;
+	}
+	else
+	{
+		// 累加静止时间，并判断是否应进入放松状态
+		IdleElapsedTime += DeltaSeconds;
+		bShouldEnterRelaxState = (IdleElapsedTime >= EnterRelaxStateThreshold);
+	}
+}
--- Source/Warrior/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
+++ Source/Warrior/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
@@ -6,6 +6,7 @@
 #include "AnimInstances/WarriorCharacterAnimInstance.h"
 #include "WarriorHeroAnimInstance.generated.h"
 
+class AWarriorHeroCharacter;
 /**
  * 
  */
@@ -13,5 +14,27 @@ UCLASS()
 class WARRIOR_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+	/** 动画初始化时调用（首次运行） */
+	virtual void NativeInitializeAnimation() override;
+
+	/** 每帧更新动画状态（线程安全） */
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
 	
+protected:
+	/** 拥有该动画实例的英雄角色指针，仅适用于英雄角色 */
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|Refrences")
+	AWarriorHeroCharacter* OwningHeroCharacter;
+
+	/** 是否应该进入放松（Relax）状态，用于站立不动太久时的动画切换 */
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool bShouldEnterRelaxState;
+
+	/** 进入放松状态所需的最小时间（单位：秒） */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float EnterRelaxStateThreshold = 5.0f;
+
+	/** 当前角色处于静止状态的累计时间 */
+	float IdleElapsedTime;
 };
