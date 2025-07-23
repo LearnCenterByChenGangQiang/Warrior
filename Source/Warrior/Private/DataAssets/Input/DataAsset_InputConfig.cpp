6fe5ccf4905db842d048178d990f2ba74de0ec89
chengreg <644076531@qq.com>
2025-07-23 13:06:09 -0400

    Set Up Hero Character: Input Config Data Asset

---
+++ Source/Warrior/Private/DataAssets/Input/DataAsset_InputConfig.cpp
@@ -0,0 +1,16 @@
+// ChenGangQiang All rights Reserved.
+
+
+#include "DataAssets/Input/DataAsset_InputConfig.h"
+
+UInputAction* UDataAsset_InputConfig::FindNativeInputActionByTag(const FGameplayTag& InputTag)
+{
+	for (const FWarriorInputActionConfig& InputActionConfig : NativeInputActions)
+	{
+		if (InputActionConfig.InputTag == InputTag && InputActionConfig.InputAction)
+		{
+			return InputActionConfig.InputAction;
+		}
+	}
+	return nullptr;
+}
